aa89e76b7d19e1003b23f2dd75a6aec3ca8c7399
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-16 10:29:35 -0600

    Add files via upload

---
+++ estaciones_ver3_tgz1_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_ver3_tgz1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Art 127 Constitucional 3935, Frutos de la Revolucion, 96470 Coatzacoalcos, Ver., México", "description": "VER3", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -94.425503, 18.1303959, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Calle 1a. Pte. Nte. 793, Colón, 29037 Tuxtla Gutiérrez, Chis.", "description": "TGZ1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -93.1158291, 16.7601904, 0.0 ] } }
+]
+}
